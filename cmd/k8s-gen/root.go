d70bd5b636e1e583404b7e5a7883767ffc6450f3
Ibrahim Diallo <20114089+thisisibrahimd@users.noreply.github.com>
2026-08-18 15:55:36 -0400

    chore: relocate compiler code for jsonschema

--- cmd/k8s-gen/root.go
+++ cmd/k8s-gen/root.go
@@ -3,18 +3,9 @@ package cmd
 import (
 	"context"
 	"log/slog"
-	"maps"
 	"os"
-	"path/filepath"
 
-	"github.com/jsonnet-libs/k8s/pkg/config"
-	"github.com/jsonnet-libs/k8s/pkg/model"
-	"github.com/jsonnet-libs/k8s/pkg/swagger"
-	"github.com/jsonnet-libs/k8s/pkg/targetgenerator"
 	"github.com/jsonnet-libs/k8s/pkg/telemetry"
-	"github.com/jsonnet-libs/k8s/pkg/util"
-	"github.com/jsonnet-libs/k8s/pkg/writer"
-	"github.com/mdobak/go-xerrors"
 	"github.com/urfave/cli/v3"
 )
 
@@ -52,104 +43,6 @@ func NewRootCommand() *cli.Command {
 	}
 
 	// action
-	cmd.Action = func(ctx context.Context, c *cli.Command) error {
-		// parse config
-		configFile := c.String("config")
-		absConfigFile, err := filepath.Abs(configFile)
-		if err != nil {
-			panic(err)
-		}
-		configDir := filepath.Dir(absConfigFile)
-		if err := os.Chdir(configDir); err != nil {
-			panic(err)
-		}
-
-		cfg, err := config.Load(absConfigFile)
-		if err != nil {
-			panic(err)
-		}
-		err = config.Validate(cfg)
-		if err != nil {
-			panic(err)
-		}
-		slog.Debug("loaded config file", slog.String("file", configFile))
-
-		// generate targets from specGenerator
-		if cfg.SpecGenerator != nil {
-			tg, err := targetgenerator.New(*cfg.SpecGenerator)
-			if err != nil {
-				return xerrors.New("failed to create target generator", err)
-			}
-
-			specs, err := tg.GenerateTargets()
-			if err != nil {
-				return xerrors.New("failed to generate targets", err)
-			}
-			cfg.Specs = specs
-		}
-
-		// inform user of filtering
-		args := c.Args().Slice()
-		if len(args) > 0 {
-			slog.Warn("filtering generation to listed versions", slog.Any("versions", args))
-		}
-
-		// generate all target in config
-		for _, t := range cfg.Specs {
-			if len(args) > 0 && !util.HasStr(args, t.Output) {
-				slog.Debug("skipping version", slog.String("version", t.Output))
-				continue
-			}
-
-			prefix := ""
-			if cfg.SpecGenerator != nil {
-				prefix = cfg.SpecGenerator.Prefix
-			}
-			if t.Prefix != "" {
-				prefix = t.Prefix
-			}
-
-			swaggerDefs := make(swagger.Definitions)
-			if len(t.Crds) > 0 {
-				for _, url := range t.Crds {
-					slog.Info("generating spec",
-						slog.String("version", t.Output),
-						slog.String("spec", url),
-						slog.String("prefix", prefix),
-					)
-
-					loadedDefs, err := swagger.Load(&swagger.CRDLoader{}, url)
-					if err != nil {
-						return xerrors.New("unable to load spec", err)
-					}
-					maps.Copy(swaggerDefs, loadedDefs)
-				}
-			} else {
-				slog.Info("generating spec",
-					slog.String("version", t.Output),
-					slog.String("spec", t.Openapi),
-					slog.String("prefix", prefix),
-				)
-
-				loadedDefs, err := swagger.Load(&swagger.SwaggerLoader{}, t.Openapi)
-				if err != nil {
-					return xerrors.New("unable to load spec", err)
-				}
-				swaggerDefs = loadedDefs
-			}
-
-			groups := model.Load(&swaggerDefs, prefix)
-			path := filepath.Join(cfg.OutputDir, t.Output)
-
-			// write libsonnet files to disk
-			diskWriter := writer.NewDiskWriter()
-			if err := diskWriter.Render(path, groups, t, cfg.LibName, cfg.Description); err != nil {
-				return xerrors.New("failed to write libsonnet files", err)
-			}
-		}
-
-		return nil
-	}
 
 	cmd.Commands = append(cmd.Commands, newGenerateCommand())
 
---
+++ pkg/complier/jsonschemacomplier/jsonschemacomplier.go
@@ -0,0 +1,119 @@
+package jsonschemacomplier
+
+import (
+	"fmt"
+
+	"github.com/jsonnet-libs/k8s/pkg/builder"
+	"github.com/jsonnet-libs/k8s/pkg/render"
+	"github.com/santhosh-tekuri/jsonschema/v6"
+	"github.com/stoewer/go-strcase"
+)
+
+func genAdditiveObjWrapper(in builder.Type, path []string) builder.Type {
+	if len(path) == 0 {
+		return in
+	}
+
+	return builder.Merge(builder.Object(path[0], genAdditiveObjWrapper(in, path[1:])))
+}
+
+func CompileLibsonnet(s *jsonschema.Schema, name string, curPath []string) builder.Type {
+	// if the current schema is just a ref to another def in another location, replace the schema in that ref with our current schema.
+	if s.Ref != nil {
+		s = s.Ref
+	}
+
+	// loop through the properties in this current schema
+	// limitations: we do not support special properties
+	propTypes := []builder.Type{}
+	for pName, p := range s.Properties {
+		newPath := make([]string, len(curPath))
+		copy(newPath, curPath)
+		newPath = append(newPath, pName)
+
+		if p.Ref != nil {
+			p = p.Ref
+		}
+
+		var pT string
+		if p.Types != nil {
+			pT = p.Types.ToStrings()[0]
+		} else {
+			if p.Enum != nil {
+				pT = "string"
+			} else if len(p.AnyOf) > 0 {
+				pT = "string"
+			}
+		}
+
+		switch pT {
+		case "array":
+			funcName := fmt.Sprintf("with%s", strcase.UpperCamelCase(pName))
+			propType := builder.Func(
+				funcName,
+				builder.Args(
+					builder.Required(builder.SafeString(strcase.LowerCamelCase(pName), "")),
+				),
+				builder.Object(
+					strcase.LowerCamelCase(pName),
+					genAdditiveObjWrapper(
+						builder.IfThenElse(
+							pName,
+							builder.Call("", "std.isArray", builder.Args(builder.Ref("v", builder.SafeIdentifier(strcase.LowerCamelCase(pName))))),
+							builder.Ref("", builder.SafeIdentifier(strcase.LowerCamelCase(pName))),
+							builder.List("", builder.Ref("", builder.SafeIdentifier(strcase.LowerCamelCase(pName)))),
+						),
+						curPath,
+					),
+				),
+			)
+			propTypes = append(propTypes, propType)
+
+			// create mixin function for ararys
+			propTypeMixin := builder.Func(
+				fmt.Sprintf("%sMixin", funcName),
+				builder.Args(
+					builder.Required(builder.SafeString(strcase.LowerCamelCase(pName), "")),
+				),
+				builder.Object(
+					strcase.LowerCamelCase(pName),
+					genAdditiveObjWrapper(
+						builder.Merge(
+							builder.IfThenElse(
+								pName,
+								builder.Call("", "std.isArray", builder.Args(builder.Ref("v", builder.SafeIdentifier(strcase.LowerCamelCase(pName))))),
+								builder.Ref("", builder.SafeIdentifier(strcase.LowerCamelCase(pName))),
+								builder.List("", builder.Ref("", builder.SafeIdentifier(strcase.LowerCamelCase(pName)))),
+							),
+						),
+						curPath,
+					),
+				),
+			)
+			propTypes = append(propTypes, propTypeMixin)
+		case "object":
+			propType := CompileLibsonnet(p, strcase.LowerCamelCase(pName), newPath)
+			propTypes = append(propTypes, propType)
+		default:
+			funcName := fmt.Sprintf("with%s", strcase.UpperCamelCase(pName))
+			propType := builder.Func(
+				funcName,
+				builder.Args(
+					builder.Required(builder.SafeString(strcase.LowerCamelCase(pName), "")),
+				),
+				builder.Object(
+					strcase.LowerCamelCase(pName),
+					genAdditiveObjWrapper(
+						builder.Ref(pName, builder.SafeIdentifier(strcase.LowerCamelCase(pName))),
+						curPath,
+					),
+				),
+			)
+			propTypes = append(propTypes, propType)
+		}
+	}
+
+	render.SortFields(propTypes)
+
+	return builder.Object(name, propTypes...)
+}
